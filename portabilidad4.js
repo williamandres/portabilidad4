d33631710dabf364f108d3a930fb7df3c1e63ae2
William Aguilar <williamaguilar@misena.edu.co>
2021-11-29 13:02:48 -0500

    add screen 4 to portabilidad 4 prolect

--- portabilidad4.js
+++ portabilidad4.js
@@ -9,8 +9,10 @@ let option2IconLineWhite = document.getElementById('simcard-option2-icon-line-wh
 
 let simCardForm = document.getElementById('simcard-form');
 let simCardForm2 = document.getElementById('simcard-form2');
+let simCardForm3 = document.getElementById('simcard-form3');
 
 let simCardbutton = document.getElementById('simcard-button');
+let simCardbutton2 = document.getElementById('simcard-button2');
 
 let simCardOptionContainer = document.getElementById('simcard-option-container')
 
@@ -44,22 +46,23 @@ function reciveDelivery()
 
 // *************************** Sim Card Option 2 ******************************
 
- simCardOption2.addEventListener('click', getShop);
+simCardOption2.addEventListener('click', getShop);
 
- function getShop()
- {
-     simCardOption2.classList.remove('simcard-option-transparent');
-     simCardOption2.classList.add('simcard-option-blue');
-     simCardOption1.classList.remove('simcard-option-blue');
-     simCardOption1.classList.add('simcard-option-transparent');
+function getShop()
+{
+    simCardOption2.classList.remove('simcard-option-transparent');
+    simCardOption2.classList.add('simcard-option-blue');
+    simCardOption1.classList.remove('simcard-option-blue');
+    simCardOption1.classList.add('simcard-option-transparent');
 
-     option2IconLineWhite.classList.remove('display-none');
-     option2IconLineBlue.classList.add('display-none');  
-     option1IconLineBlue.classList.remove('display-none');
-     option1IconLineWhite.classList.add('display-none');
- }
+    option2IconLineWhite.classList.remove('display-none');
+    option2IconLineBlue.classList.add('display-none');  
+    option1IconLineBlue.classList.remove('display-none');
+    option1IconLineWhite.classList.add('display-none');
+}
 
 // *************************** /Sim Card Option 2 ******************************
+
 
 
 
@@ -72,4 +75,25 @@ function screen2()
 {
     simCardForm2.classList.remove('display-none');
     simCardForm.classList.add('display-none');
+    simCardForm3.classList.add('display-none');
 }
+
+// *************************** /Sim Card Option 1/ screen 2  ******************************
+
+
+
+
+
+// *************************** Sim Card Option 1/ screen 3  ******************************
+
+simCardbutton2.addEventListener('click', screen3);
+
+function screen3()
+{
+    simCardForm3.classList.remove('display-none');
+    simCardForm2.classList.add('display-none');
+    simCardForm.classList.add('display-none');
+}
+
+
+// *************************** /Sim Card Option 1/ screen 2  ******************************
